350c4662f8903b40eb64c085815827f1cc2af807
Nimra2212 <nimrakhan2212@gmail.com>
2021-02-02 14:03:47 +0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'bulb-on';
 
-
+  bgImg=''
   bulb=false;
-
+ngOnInit(){
+  this.bgImg="url('./../../../assets/bulb.jpg')"
+}
   button(){
     if(this.bulb==false){
      this.bulb=true;
